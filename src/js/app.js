dbed81e70a5ca597a32c7770659d63bd81f3d5da
Costin Soare <soarecostin@gmail.com>
2019-01-11 21:46:42 +0100

    Remove jquery (unused)

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-
+    // window.Popper = require('popper.js').default;
+    // window.$ = window.jQuery = require('jquery');
+    
     require('bootstrap');
 } catch (e) {}
